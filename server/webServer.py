ae49cd8761dfb2a13b8eaa6dc40a464823b3b337
dajacobs <dajacobs.tab@gmail.com>
2015-10-22 09:46:53 -0500

    update

--- server/webServer.py
+++ server/webServer.py
@@ -4,8 +4,8 @@
 from socket import *
 # Socket/Port variables
 serverSocket = socket(AF_INET, SOCK_STREAM)
-TCP_PORT     = 8000
-BUFFER_SIZE  = 1024
+TCP_PORT = 8000
+BUFFER_SIZE = 1024
 
 # Bind TCP set and set socket to listen
 serverSocket.bind(('', TCP_PORT))
@@ -20,19 +20,16 @@ while True:
 	# Try block for HTTP request/response/error message
 	try:
 		message = connectionSocket.recv(BUFFER_SIZE)
-		file    = message.split()[1]
-		f       = open(file[1:])
-		out     = f.read()
+		file = message.split()[1]
+		f = open(file[1:])
+		out = f.read()
 
 		# HTTP header/file content sent to client
-		connectionSocket.send('HTTP/1.0 200 OK\r\n')
+		connectionSocket.send('\nHTTP/1.0 200 OK\r\n')
 		for i in range(0, len(out)):
 			connectionSocket.send(out[i])
 		connectionSocket.close()	
 	except IOError:
 		# 404 Not Found Error Message
-		err = '''<html> <head> <title> 404 </title> </head> <body><h1>404 Not Found</h1> <h3> Error Message </h3> </body></html>'''
-		connectionSocket.send('HTTP/1.0 200 OK\r\n'%len(err))
-		for i in err:
-			connectionSocket.send(i)
+		connectionSocket.send('\n404 Not Found\n')
 	serverSocket.close()						  		
